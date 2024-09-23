29a0f8d54386b4b675a9033827974ccb81808d86
Daniel <100704382+Ristler@users.noreply.github.com>
2024-09-23 10:45:02 +0300

    Tietokanta toimii nyt

--- game.py
+++ game.py
@@ -1,2 +1,13 @@
-##It starts here
-print("mun branch")
+import mysql.connector
+
+yhteys = mysql.connector.connect(
+    host='127.0.0.1',
+    port=3306,
+    database='peli',
+    user='root',
+    password='salasana',
+    autocommit=True
+)
+
+
+
